1c5fe4864d20215913beda28f4e53424d9b4f420
GiselaFlores <gi.flores@bue.edu.ar>
2023-04-18 21:17:50 -0300

    import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from './Componentes/Home';
+import About from './Componentes/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <h1>MENU</h1>
+      <Navbar /> 
+      <Cabecera />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+
     </div>
   );
 }
